9d6aa6d500c29804aaef578852aef2bc06e54044
vecxelwizard <theo@matrixdesigns.co.za>
2017-09-18 12:20:54 +0200

    Project complete ...

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,16 @@
 package com.londonappbrewery.magiceightball;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView ballDisplay = (ImageView) findViewById(R.id.image_eightBall);
+
+        final int[] ballArray = {
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+        Button myButton;
+        myButton = (Button) findViewById(R.id.askButton);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("8Ball", "button pressed");
+                Random randomNumberGenetator = new Random();
+                int number;
+                number = randomNumberGenetator.nextInt(5);
+                Log.d("8Ball", "The random number is: " + number);
+                ballDisplay.setImageResource(ballArray[number]);
+
+            }
+        });
+
     }
 }
